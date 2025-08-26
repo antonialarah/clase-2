d7a3e07930a7d0a2109689b56522e9818670dcfe
antonia lara <antonialarahern@gmail.com>
2025-08-25 21:18:08 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-     var valor = document.getElementById("pecinco").offsetWidth;
+var valor = document.getElementById("pecinco").offsetWidth;
 
             window.addEventListener("resize", function () {
                 valor = document.getElementById("pecinco").offsetWidth;
@@ -6,20 +6,21 @@
 
             function setup() {
                 createCanvas(valor, valor, WEBGL).parent("#pecinco");
-                select("#nombre").html("Antonia Lara");
-                select("#aprendizaje").html("a usar programas de adobe");
-                noStroke();
+                select("#nombre").html("Vicente Medina");
+                select("#aprendizaje").html("a hacer paginas web y aprender mas de Photoshop e Illustrator :P");
+                noStroke()
             }
             function draw() {
                 background(255, 216, 223);
                 orbitControl();
-                pointLight(255, 255, 255, 50)
+                pointLight(255, 255, 255, valor/3, valor/2, valor/50)
+                cone(valor / 3, valor/5);
                 specularMaterial(255)
-                shininess(50);
-                metalness(1);
-                fill(255,255,255)
-                cylinder(valor / 2, valor/3);
+                shininess(50)
+                metalness(1)
+                fill(70,255,20)
             }
+            
 
             function windowResized() {
                 resizeCanvas(valor, valor);
